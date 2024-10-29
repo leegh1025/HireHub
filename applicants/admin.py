5166320b11e0a2e300fecff4e02637cb406470f5
leegh1025 <leegh1025@naver.com>
2024-10-29 10:56:28 +0900

    [추가]admin 필터기능

--- applicants/admin.py
+++ applicants/admin.py
@@ -4,7 +4,6 @@ from template.models import EvaluationTemplate, InterviewTemplate, InterviewQues
 
 # Register your models here.
 
-admin.site.register(Application)
 admin.site.register(ApplicationTemplate)
 admin.site.register(ApplicationQuestion)
 admin.site.register(EvaluationTemplate)
@@ -13,3 +12,9 @@ admin.site.register(InterviewQuestion)
 admin.site.register(Answer)
 admin.site.register(Possible_date_list)
 admin.site.register(Applicant)
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'applicant', 'template', 'status', 'is_drafted', 'submission_date')
+    list_filter = ('is_drafted', 'status')  # 임시저장 여부와 상태 필터 추가
+    search_fields = ('applicant__username', 'name', 'template__name')
